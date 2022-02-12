1c0a333959f9cde0add74df43182658abb63f361
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-12 17:18:00 -0300

    Create class to MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -6,51 +6,63 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.all(10),
-          child: FloatingActionButton(
-            onPressed: () {},
-            child: const Center(
-              child: Icon(
-                Icons.add,
-                size: 25,
-                color: Colors.blue,
-              ),
+    return MediaQuery(
+      data: const MediaQueryData(),
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.grey,
+          ),
+          home: const MyHomePage()),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(10),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Center(
+            child: Icon(
+              Icons.add,
+              size: 25,
+              color: Colors.blue,
             ),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         ),
-        appBar: AppBar(
-          leading: const Icon(Icons.history),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Icon(Icons.info_outline),
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 15, left: 100),
-          child: Column(
-            children: const [
-              Text(
-                "Your Widgets",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-            ],
+      ),
+      appBar: AppBar(
+        leading: const Icon(Icons.history),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.info_outline),
+          )
+        ],
+      ),
+      body: Column(
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(top: 15, left: 100),
+            child: Text(
+              "Your Widgets",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
