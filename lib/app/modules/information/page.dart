132fe992383a94d159fbfc858bdbed78184af8c3
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-15 21:47:07 -0300

    Add page information data

--- lib/app/modules/information/page.dart
+++ lib/app/modules/information/page.dart
@@ -5,6 +5,34 @@ class InformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Information screen");
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Information"),
+        ),
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "How to Share a Widget",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Column(
+                  children: const [
+                    Text(
+                        "Step 1. Create a new widget. On the main screen, click the plus button then \"Create a new widget\". Once the widget is created you will be given the Widget ID"),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )));
   }
 }
