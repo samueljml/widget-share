28ef5cc2c915c50cc54a382100a8efb257b1837d
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-18 21:54:53 -0300

    Add history item structure

--- lib/app/modules/history/page.dart
+++ lib/app/modules/history/page.dart
@@ -11,15 +11,45 @@ class HistoryScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 0.95,
-            height: MediaQuery.of(context).size.height * 0.6,
-            decoration: BoxDecoration(
-                color: Colors.black, borderRadius: BorderRadius.circular(40)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.width * 0.95,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(40)),
+              ),
+            ],
           ),
+          const SizedBox(height: 15),
+          ChoiceChip(
+              pressElevation: 0.0,
+              selectedColor: Colors.blue,
+              backgroundColor: Colors.grey[100],
+              label: const Text(
+                "6 hours ago",
+                style: TextStyle(color: Colors.white),
+              ),
+              selected: true,
+              onSelected: (bool selected) {}),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.delete_outline)),
+              const SizedBox(width: 15),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+              const SizedBox(width: 15),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.file_download_outlined)),
+            ],
+          )
         ],
       ),
     );
