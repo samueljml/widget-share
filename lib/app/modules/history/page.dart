3d0f7fe2d082aeb74cbda73a565c72632dee7f4e
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-19 09:54:57 -0300

    Add scroll view to history and add item list

--- lib/app/modules/history/page.dart
+++ lib/app/modules/history/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_share/app/modules/history/item.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -10,47 +11,16 @@ class HistoryScreen extends StatelessWidget {
         title: const Text("History"),
         elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.95,
-                height: MediaQuery.of(context).size.width * 0.95,
-                decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(40)),
-              ),
-            ],
-          ),
-          const SizedBox(height: 15),
-          ChoiceChip(
-              pressElevation: 0.0,
-              selectedColor: Colors.blue,
-              backgroundColor: Colors.grey[100],
-              label: const Text(
-                "6 hours ago",
-                style: TextStyle(color: Colors.white),
-              ),
-              selected: true,
-              onSelected: (bool selected) {}),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.delete_outline)),
-              const SizedBox(width: 15),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
-              const SizedBox(width: 15),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.file_download_outlined)),
-            ],
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 15),
+        child: Wrap(
+          runSpacing: 15,
+          children: const [
+            Item(),
+            Item(),
+            Item(),
+          ],
+        ),
       ),
     );
   }
