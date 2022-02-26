db7bcf6b14e379c224c5286a7c1b68d9ccc489eb
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-26 17:05:26 -0300

    Add expandable panel to information screen

--- lib/app/modules/information/page.dart
+++ lib/app/modules/information/page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:widget_share/app/global_widgets/expandable_panel.dart';
 import 'package:widget_share/app/routes/Routes.dart';
 
-class InformationScreen extends StatelessWidget {
+class InformationScreen extends StatefulWidget {
   const InformationScreen({Key? key}) : super(key: key);
 
+  @override
+  State<InformationScreen> createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  bool isShowing = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,26 +31,37 @@ class InformationScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "How to Share a Widget",
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 21),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 5,
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Step 1. Create a new widget. On the main screen, click the plus button then \"Create a new widget\". Once the widget is created you will be given the Widget ID\n\nStep 2. Send the Widget ID to your friend.\n\nStep 3. Your friend shoud add the Widget ID to their app. On the main screen, click the plus button then \"Add a friend's widget\".\n\nStep 4. That's it! You can now both send pictures to the same widget.",
-                      style: Theme.of(context).textTheme.bodyText1,
+              ExpandablePanelComponent(options: [
+                ExpandablePanelItem(
+                    header: Row(
+                      children: const [
+                        Text(
+                          "How to Share a Widget",
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
+                    body: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Step 1. Create a new widget. On the main screen, click the plus button then \"Create a new widget\". Once the widget is created you will be given the Widget ID\n\nStep 2. Send the Widget ID to your friend.\n\nStep 3. Your friend shoud add the Widget ID to their app. On the main screen, click the plus button then \"Add a friend's widget\".\n\nStep 4. That's it! You can now both send pictures to the same widget.",
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ],
+                      ),
+                    ),
+                    isExpanded: isShowing,
+                    setIsExpanded: (bool isExpanded) {
+                      setState(() {
+                        isShowing = isExpanded;
+                      });
+                    }),
+              ]),
             ],
           ),
         )));
