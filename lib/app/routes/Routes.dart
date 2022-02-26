0d5be1ea5d06c6c79ca5355527c87b91a3e6a7b1
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-25 21:22:09 -0300

    Create abstract class to routes

---
+++ lib/app/routes/Routes.dart
@@ -0,0 +1,6 @@
+abstract class Routes {
+  static const information = "/information";
+  static const history = "/history";
+  static const newWidget = "/new-widget";
+  static const settings = "/settings";
+}
